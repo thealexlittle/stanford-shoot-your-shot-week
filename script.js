df1bf3b7e060e8ab40ae329330ed9fbebbdf6fb3
thealexlittle <alexlittle2college@gmail.com>
2020-01-23 00:34:47 -0800

    The site has been started

---
+++ script.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCgNLqBfLX0Svq5ecW6PjLqK0N-Btz_FzQ",
+  authDomain: "stanford-shoot-your-shot-week.firebaseapp.com",
+  databaseURL: "https://stanford-shoot-your-shot-week.firebaseio.com",
+  projectId: "stanford-shoot-your-shot-week",
+  storageBucket: "stanford-shoot-your-shot-week.appspot.com",
+  messagingSenderId: "481364868087",
+  appId: "1:481364868087:web:a2922e15b08ce385fc0cf2",
+  measurementId: "G-FQNBN774RL"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
