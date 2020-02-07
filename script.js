e0ac26e07f6b891adcd89882d5d63920bf324020
apandit42 <apandit@stanford.edu>
2020-02-07 22:12:50 +0000

    tingman

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyCgNLqBfLX0Svq5ecW6PjLqK0N-Btz_FzQ",
-  authDomain: "stanford-shoot-your-shot-week.firebaseapp.com",
-  databaseURL: "https://stanford-shoot-your-shot-week.firebaseio.com",
-  projectId: "stanford-shoot-your-shot-week",
-  storageBucket: "stanford-shoot-your-shot-week.appspot.com",
-  messagingSenderId: "481364868087",
-  appId: "1:481364868087:web:a2922e15b08ce385fc0cf2",
-  measurementId: "G-FQNBN774RL"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+"use strict";
 
-const db = firebase.firestore();
+
+
+// let firebaseConfig = {
+//   apiKey: "AIzaSyCgNLqBfLX0Svq5ecW6PjLqK0N-Btz_FzQ",
+//   authDomain: "stanford-shoot-your-shot-week.firebaseapp.com",
+//   databaseURL: "https://stanford-shoot-your-shot-week.firebaseio.com",
+//   projectId: "stanford-shoot-your-shot-week",
+//   storageBucket: "stanford-shoot-your-shot-week.appspot.com",
+//   messagingSenderId: "481364868087",
+//   appId: "1:481364868087:web:a2922e15b08ce385fc0cf2",
+//   measurementId: "G-FQNBN774RL"
+// };
+//
+// firebase.initializeApp(firebaseConfig);
+//
+// const db = firebase.firestore();
 
 // TODO: Add Shot Counter
 // Increment Counter
